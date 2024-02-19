45bdded652a3b9d78c603d0d4620d1a4e8570c0f
PK <pkjmesra@gmail.com>
2024-02-20 00:33:14 +0100

    bug fix

--- pkscreener/classes/Utility.py
+++ pkscreener/classes/Utility.py
@@ -161,9 +161,9 @@ class tools:
         if "RUNNER" in os.environ.keys():
             return
         if platform.system() == "Windows":
-            os.system("cls >/dev/null 2>&1")
+            os.system("cls")
         else:
-            os.system("clear >/dev/null 2>&1")
+            os.system("clear")
         try:
             print(art.encode('utf-8').decode(STD_ENCODING))
         except Exception as e:# pragma: no cover
